4d30255a8319bb636660d6da9b3b48d39b30ed63
Acaua Sperl de Faria <acaua.faria@gmail.com>
2019-01-22 19:14:06 -0200

    Fix DeprecationWarning for mongoose.connect

--- index.js
+++ index.js
@@ -14,11 +14,16 @@ app.use("/books", books);
 
 const port = process.env.PORT || 5000;
 
-mongoose.connect(dbstring).then(
-  () => {
-    app.listen(port, () => console.log(`Server listening on port ${port}`));
-  },
-  err => {
-    console.error("Cannot connect to db", err);
-  }
-);
+mongoose
+  .connect(
+    dbstring,
+    { useNewUrlParser: true }
+  )
+  .then(
+    () => {
+      app.listen(port, () => console.log(`Server listening on port ${port}`));
+    },
+    err => {
+      console.error("Cannot connect to db", err);
+    }
+  );
